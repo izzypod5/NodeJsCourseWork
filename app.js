7dafac2fa25f9430a1d52da66ef19c483fe52388
Adam Letch <izzypod5@googlemail.com>
2020-06-29 12:52:45 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const adminRoutes = require("./routes/admin");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(adminRoutes);
+
+app.use("/", (req, res, next) => {
+  res.send("<h1>Hello from Express</h1>");
+});
+
+app.listen(3000);
