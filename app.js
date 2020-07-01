9ca8981cab508489d8485bb3377a17f9b05920c2
Adam Letch <izzypod5@googlemail.com>
2020-07-01 11:47:02 +0100

    Added EJS templating

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const adminData = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
-
 const app = express();
 
-app.set("view engine", "pug");
+app.set("view engine", "ejs");
 app.set("views", "views");
+
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
